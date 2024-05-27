e35b8336f652efb4e79f32ce962c651f098b6a37
mbuze <maciej.buze@gmail.com>
2024-05-27 09:55:02 +0100

    adjust sysimage file

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -9,6 +9,6 @@ create_sysimage([:Pluto, :PlutoUI,
                  :GaussianProcesses, :CondaPkg];
                 #precompile_execution_file = "warmup.jl",
                 precompile_statements_file = "combined_trace.jl",
-                #replace_default = true,
+                replace_default = true,
                 sysimage_path="/home/jovyan/sysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
